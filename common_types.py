20edd0fef7ba20ffc5d436e9a16b44b4cdb04951
202516531 <jjcarandang1@up.edu.ph>
2026-02-27 12:08:34 +0800

    feat: add model template

--- common_types.py
+++ common_types.py
@@ -1,2 +1,16 @@
-from argparse import ArgumentParser
-from model import ConnectTacToeModel
+# pyright: strict
+
+from enum import Enum, auto
+
+class Player(Enum):
+    P1 = auto()
+    P2 = auto()
+
+class WinConditionType(Enum):
+    NOT_CONNECT_FOUR = auto()
+    TIC_TAC_TOE = auto()
+
+class TokenPhysicsType(Enum):
+    FLOATING = auto()
+    STRONG_GRAVITY = auto()
+    WEAK_GRAVITY = auto()
--- model.py
+++ model.py
@@ -1,4 +1,35 @@
 # pyright: strict
 
+from common_types import Player, WinConditionType, TokenPhysicsType
+
+
 class ConnectTacToeModel:
-    ...
+    def __init__(self):
+        # will leave this for later
+        pass
+
+    @property
+    def current_player(self) -> Player:
+        pass
+
+    @property
+    def winner(self) -> Player | None:
+        pass
+
+    @property
+    def is_game_done(self) -> bool:
+        pass
+
+    def choose_cell(self, row: int, col: int) -> bool:
+        pass
+
+    @property
+    def row_count(self) -> int:
+        return 6 
+
+    @property
+    def col_count(self) -> int:
+        return 7
+
+    def get_owner(self, row: int, col: int) -> Player | None:
+        pass
