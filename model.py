3aca82659643f3cea38b5a4d374b54df03de7fc0
Jowee <ja.magturo@gmail.com>
2026-02-27 12:46:08 +0800

    Update ConnectTacToeModel with grid setup and properties
    Refactor grid initialization and add game state properties.

--- model.py
+++ model.py
@@ -4,25 +4,20 @@ from common_types import Player, WinConditionType, TokenPhysicsType
 
 
 class ConnectTacToeModel:
+    ROWS, COLUMNS = 6, 7
+    
     def __init__(self, game_mode: WinConditionType, physics: TokenPhysicsType):
         self._game_mode: WinConditionType = game_mode
         self._physics: TokenPhysicsType = physics
 
-        self._grid: list[list[str]] = []
-        # making the grid
-        curr_row: list[str] = []
-        for r in range(self.row_count):
-            curr_row = []
-            for c in range(self.col_count):
-                curr_row.append('.')
-            self._grid.append(curr_row)
-
+        self._grid = [[None for _ in range(self.COLUMNS)] for _ in range(self.ROWS)]
         self._winner: Player | None = None
         self._current_player = Player
+        self._is_game_done = False
 
     @property
     def current_player(self) -> Player:
-        pass
+        return self._current_player
 
     @property
     def winner(self) -> Player | None:
@@ -30,22 +25,53 @@ class ConnectTacToeModel:
 
     @property
     def is_game_done(self) -> bool:
-        if game_mode == WinConditionType.TIC_TAC_TOE:
+        return self._is_game_done
+    
+    @property
+    def row_count(self):
+        return self.ROWS
 
-
-        else:
-            pass
+    @property
+    def col_count(self):
+        return self.COLUMNS
 
     def choose_cell(self, row: int, col: int) -> bool:
-        pass
+        if self._is_game_done:
+            return False
+        if (0 <= row <= SELF.ROWS) or (0 <= col <= SELF.COLUMNS) 
+            return False
+        if self._grid[row][col] is not None:
+            return False
 
-    @property
-    def row_count(self) -> int:
-        return 6 
+        self._grid[row][col] = self._current_player
+        self.winner_checker()
 
-    @property
-    def col_count(self) -> int:
-        return 7
+        if grid_checker and not self._is_game_done:
+            self._is_game_done = True
+            return False
 
+        if self._is_game_done:
+            return False
+
+        if not self._is_game_done:
+            self.player_turn
+
+        return True
+        
     def get_owner(self, row: int, col: int) -> Player | None:
-        pass
+        return self._grid[row][col] if self._grid[row][col] is not None else None
+
+    def winner_checker(self):
+        pass   
+        
+    def grid_checker(self):
+        return all(self._grid[row][col] is not None
+            for row in range(self.ROWS) 
+            for col in range(self.COLUMNS)
+            )
+
+    def player_turn(self, player):
+        if self.current_player == Player.P1:
+            self._current_player = Player.P2
+        else:
+            self._current_player = Player.P1
