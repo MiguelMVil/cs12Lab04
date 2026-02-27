a06154af078dfcd6051a2b0a8a803232fcb96076
egsoliva <egsoliva06@gmail.com>
2026-02-27 12:59:14 +0800

    added controller to main

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 from argparse import ArgumentParser
 from model import ConnectTacToeModel
+from view import ConnectTacToeView
+from common_types import Player
+
+class ConnectTacToeController:
+    def __init__(self, model: ConnectTacToeModel, view: ConnectTacToeView):
+        self._model = model
+        self._view = view
+
+    def run(self):
+        model = self._model
+        view = self._view
+
+        while not model.is_game_done:
+            if model.current_player == Player.P1:
+                ...
+            else:
+                ...
+            ...
+
+# if __name__ == "__main__":
+#     model = ConnectTacToeModel()
